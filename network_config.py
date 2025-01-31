8d78bea52fcb09f9f55e37524c8da40ff5bbf741
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2025-02-01 05:01:08 +0700

    Update network_config.py

--- network_config.py
+++ network_config.py
@@ -1,15 +1,37 @@
-# network_config.py
-
 networks = {
-    'Base': {
-        'rpc_url': 'https://sepolia.base.org', 
-        'chain_id': 84532, 
-        'contract_address': 
-        '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5' 
+    'l1rn': {
+        'rpc_url': 'https://brn.rpc.caldera.xyz/http',
+        'chain_id': 6636130,  # Sesuaikan dengan chain ID untuk L1RN
+        'contract_address': '',  
+        'shortcut': 'l1rn',
+        'enable_network': 'l1rn'
     },
-    'OP Sepolia': {
+    'Arbitrum Sepolia': {
+        'rpc_url': 'https://sepolia-rollup.arbitrum.io/rpc',
+        'chain_id': 421613,
+        'contract_address': '0x8D86c3573928CE125f9b2df59918c383aa2B514D',
+        'shortcut': 'arbt',
+        'enable_network': 'arbitrum-sepolia'
+    },
+    'Base Sepolia': {
+        'rpc_url': 'https://sepolia.base.org',
+        'chain_id': 84532,
+        'contract_address': '0x30A0155082629940d4bd9Cd41D6EF90876a0F1b5',
+        'shortcut': 'bssp',
+        'enable_network': 'base-sepolia'
+    },
+    'Blast Sepolia': {
+        'rpc_url': 'https://sepolia.blast.io',
+        'chain_id': 168587773,
+        'contract_address': '0x1D5FD4ed9bDdCCF5A74718B556E9d15743cB26A2',
+        'shortcut': 'blss',
+        'enable_network': 'blast-sepolia'
+    },
+    'Optimism Sepolia': {
         'rpc_url': 'https://endpoints.omniatech.io/v1/op/sepolia/public',
         'chain_id': 11155420,
-        'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38'
-    },
+        'contract_address': '0xF221750e52aA080835d2957F2Eed0d5d7dDD8C38',
+        'shortcut': 'opsp',
+        'enable_network': 'optimism-sepolia'
+    }
 }
