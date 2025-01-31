332436b3e3c0e623793f29b085f575eed3ac2a25
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2025-02-01 04:31:05 +0700

    Update keys_and_addresses.py

--- keys_and_addresses.py
+++ keys_and_addresses.py
@@ -4,4 +4,4 @@
 PRIVATE_KEY = "0xYOUR_PRIVATE_KEY_HERE"
 
 # Alamat tujuan untuk bridging (sama dengan alamat dompet)
-DESTINATION_ADDRESS = "0x0e80E657EbD638EaE749387bc4b468993EDc0d5B"
+DESTINATION_ADDRESS = ""
